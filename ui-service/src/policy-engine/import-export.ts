96d68b54ee16c85c2014ba50c51967a23679ba34
Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>
2022-01-12 21:58:37 +0400

    fix import tokens
    Signed-off-by: Stepan Kiryakov <stepan.kiryakov@envisionblockchain.com>

--- ui-service/src/policy-engine/import-export.ts
+++ ui-service/src/policy-engine/import-export.ts
@@ -90,11 +90,14 @@ importExportAPI.post('/import', async (req: AuthenticatedRequest, res: Response)
 
         const existingTokens = await guardians.getTokens({});
         const existingSchemas = await guardians.getSchemes({});
+        
+        const existingTokensMap = {};
+        existingTokens.forEach(token => existingTokensMap[token.tokenId] = true);
+        tokens = tokens.filter((token:any) => !existingTokensMap[token.tokenId]);
         for (let token of tokens) {
             delete token.selected;
             delete token.id;
         }
-        tokens = tokens.filter((token:any) => !existingTokens.includes(token.tokenId));
 
         for (let schema of schemas) {
             const oldUUID = schema.uuid;
